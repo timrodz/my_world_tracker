899687b06d499152ac0a5259a77c9142b22e9bef
Juan Morais <timrodz@icloud.com>
2026-03-25 22:48:39 +1300

    fix: NPM packages

--- config/config.exs
+++ config/config.exs
@@ -59,9 +59,15 @@ config :esbuild,
   version: "0.25.4",
   world_tracker: [
     args:
-      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --loader:.png=file --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => [Path.expand("../assets/node_modules", __DIR__), Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
+    env: %{
+      "NODE_PATH" => [
+        Path.expand("../assets/node_modules", __DIR__),
+        Path.expand("../deps", __DIR__),
+        Mix.Project.build_path()
+      ]
+    }
   ]
 
 # Configure tailwind (the version is required)
